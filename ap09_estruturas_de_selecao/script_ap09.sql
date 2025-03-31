bb92aec6b8c31bd3c0084f42c80994bedcd92ff3
Julia Augustinho <julia.augustinho@fatec.sp.gov.br>
2025-03-31 18:55:16 -0300

    feat: adiciona estrutura de selecao para avaliar numero inteiro multiplo de 3 ou de 5

--- ap09_estruturas_de_selecao/script_ap09.sql
+++ ap09_estruturas_de_selecao/script_ap09.sql
@@ -41,3 +41,36 @@ BEGIN
 END;
 $$
 
+-- 1.2 Faça um programa que exibe se um número inteiro é múltiplo de 3 ou de 5.
+-- SOLUÇÃO 1.2 - Com apenas IF e suas variações
+DO $$
+DECLARE
+    -- Gerar um número inteiro aleatório entre 1 e 100
+    numero INT:= valor_aleatorio_entre(1, 100);
+BEGIN
+    -- Verificar se esse número é múltiplo de 3 ou de 5, o que significa que ao dividir esse número por 3 ou 5, o resto da divisão é zero.
+    -- SE o resto da divisão por 3 ou 5 do numero der 0, então é múltiplo. 
+    IF numero % 3 = 0 OR numero % 5 = 0 THEN
+        RAISE NOTICE 'O numero % é múltiplo de 3 ou de 5.', numero;
+    ELSE
+        RAISE NOTICE 'O numero % não é múltiplo de 3 e nem de 5.', numero;
+    END IF;
+END;
+$$
+
+-- SOLUÇÃO 2.2 - Com apenas CASE e suas variações
+DO $$
+DECLARE
+    -- Gerar um número inteiro aleatório entre 1 e 100
+    numero INT:= valor_aleatorio_entre(1, 100);
+BEGIN
+    -- Verificar se esse número é múltiplo de 3 ou de 5, o que significa que ao dividir esse número por 3 ou 5, o resto da divisão é zero.
+    -- SE o resto da divisão por 3 ou 5 do numero der 0, então é múltiplo. 
+    CASE
+        WHEN (numero % 3 = 0) OR (numero % 5 = 0) THEN
+            RAISE NOTICE 'O numero % é múltiplo de 3 ou de 5.', numero;
+        ELSE
+            RAISE NOTICE 'O numero % não é múltiplo de 3 e nem de 5.', numero;
+    END CASE;
+END;
+$$
