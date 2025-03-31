36bf40cb43a94997e3bc0cea447d3365efb72f47
Julia Augustinho <julia.augustinho@fatec.sp.gov.br>
2025-03-31 16:39:07 -0300

    feat: gera estrutura de selecao para avaliar numero inteiro multiplo de 3

---
+++ script_ap09.sql
@@ -0,0 +1,42 @@
+-- Criando a função de valor aleatório
+CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+ INT) RETURNS INT AS
+ $$
+ BEGIN
+ RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+ END;
+ $$ LANGUAGE plpgsql;
+
+-- 1.1 Faça um programa que exibe se um número inteiro é múltiplo de 3
+-- SOLUÇÃO 1.1 - Com apenas IF e suas variações
+DO $$
+DECLARE
+    -- Gerar um número inteiro aleatório entre 1 e 30
+    numero INT:= valor_aleatorio_entre(1, 30);
+BEGIN
+    -- Verificar se esse número é múltiplo de 3, o que significa que ao dividir esse número por 3, o resto da divisão é zero.
+    -- SE o resto da divisão por 3 do numero der 0, então é múltiplo. 
+    IF numero % 3 = 0 THEN
+        RAISE NOTICE 'O numero % é múltiplo de 3', numero;
+    ELSE
+        RAISE NOTICE 'O numero % não é múltiplo de 3', numero;
+    END IF;
+END;
+$$
+
+-- SOLUÇÃO 2.1 - Com apenas CASE e suas variações
+DO $$
+DECLARE
+    -- Gerar um número inteiro aleatório entre 1 e 30
+    numero INT:= valor_aleatorio_entre(1, 30);
+BEGIN
+    -- Verificar se esse número é múltiplo de 3, o que significa que ao dividir esse número por 3, o resto da divisão é zero.
+    -- SE o resto da divisão por 3 do numero der 0, então é múltiplo. 
+    CASE
+        WHEN numero % 3 = 0 THEN
+            RAISE NOTICE 'O numero % é múltiplo de 3', numero;
+        ELSE
+            RAISE NOTICE 'O numero % não é múltiplo de 3', numero;
+    END CASE;
+END;
+$$
